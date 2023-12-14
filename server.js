d1e83e69b6dba990cf338db78c1bdd6ef95ebe8a
Max Schneider <maxschneidercodes@gmail.com>
2023-12-14 08:32:17 +0100

    added quality

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const ytdl = require("ytdl-core");
 const fs = require("fs");
-const videoId =
-  "https://www.youtube.com/watch?v=rKdRj4nVqqg&ab_channel=MarkDeJesus";
-// Get video info from YouTube
+const videoId = "https://www.youtube.com/shorts/4xii9btD3jk?feature=share";
+
 ytdl
   .getInfo(videoId)
   .then((info) => {
-    // Select the video format and quality
-    const format = ytdl.chooseFormat(info.formats, { quality: "18" });
-    // Create a write stream to save the video file
+    const format = ytdl.chooseFormat(info.formats, { quality: "136" });
     const outputFilePath = `${info.videoDetails.title}.mp4`;
     const outputStream = fs.createWriteStream(outputFilePath);
-    // Download the video file
+
     ytdl.downloadFromInfo(info, { format: format }).pipe(outputStream);
-    // When the download is complete, show a message
+
+    outputStream.on("open", (chunck) => {
+      console.log("downloading..");
+    });
+
     outputStream.on("finish", () => {
       console.log(`Finished downloading: ${outputFilePath}`);
     });
